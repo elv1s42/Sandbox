22037b3d85af09464c7bb8faa23c7a3ee9747f03
Kosjakov Evgeniy <42_@mail.ru>
2016-01-29 14:40:55 +0300

    test several solutions

---
+++ TestSandbox9/TestSandbox9/Class1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace TestSandbox9
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test()
+        {
+
+        }
+    }
+}
