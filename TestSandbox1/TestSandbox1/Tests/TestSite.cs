288a9866c09b2f4eef394541f4469dc874a589cd
Kosjakov Evgeniy <42_@mail.ru>
2016-02-04 15:27:47 +0300

    testing selenium

--- TestSandbox1/TestSandbox1/Tests/TestSite.cs
+++ TestSandbox1/TestSandbox1/Tests/TestSite.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Text;
+using System.Linq;
 using System.Threading;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Firefox;
 
 namespace TestSandbox1.Tests
 {
@@ -30,15 +29,23 @@ namespace TestSandbox1.Tests
         {
             Console.WriteLine("Test started!");
             const string mailUrl = "https://10minutemail.net";
-            const string sportsUrl = "https://sports.ru/";
+            //const string sportsUrl = "https://sports.ru/";
+            const string gitUrl = "https://github.com/join";
             using (D)
             {
                 D.Navigate().GoToUrl(mailUrl);
                 var email = D.FindElement(By.Id("fe_text")).Text;
+                var username = email.Split('@').First();
+                var password = username + "www12342";
                 Console.WriteLine("Email = '{0}'", email);
-                D.Navigate().GoToUrl(sportsUrl);
-                //D.W
-                Thread.Sleep(10000);
+                //D.Navigate().GoToUrl(sportsUrl);
+                //Thread.Sleep(10000);
+                D.Navigate().GoToUrl(gitUrl);
+                D.FindElement(By.Id("user_login")).SendKeys(username);
+                D.FindElement(By.Id("user_email")).SendKeys(email);
+                D.FindElement(By.Id("user_password")).SendKeys(password);
+
+                Thread.Sleep(5000);
             }
         }
     }
