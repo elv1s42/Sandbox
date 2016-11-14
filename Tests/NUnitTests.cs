457ea34a808ecfb2776a56d6a7de72bf2fcf76a3
Evgeniy Kosjakov <42_@mail.ru>
2016-11-15 00:11:42 +0300

    remake

--- Tests/NUnitTests.cs
+++ Tests/NUnitTests.cs
@@ -9,10 +9,11 @@ namespace Tests
         [TestFixture]
         public class Sandbox
         {
-            [Test, NunitGoAction("ab396a20-b3a0-4920-8bc4-26545e9f3803")]
+            [Test]
+            //[Test, NunitGoAction("ab396a20-b3a0-4920-8bc4-26545e9f3803")]
             public void Test()
             {
-                EmailSender.Test();
+                //EmailSender.Test();
             }
         }
     }
