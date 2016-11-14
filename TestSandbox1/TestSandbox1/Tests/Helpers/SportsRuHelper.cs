2cf9d7b0af113b907a74b5af8a62e9a08bca856e
Evgeniy Kosjakov <42_@mail.ru>
2016-11-14 23:40:00 +0300

    changes

--- TestSandbox1/TestSandbox1/Tests/Helpers/SportsRuHelper.cs
+++ TestSandbox1/TestSandbox1/Tests/Helpers/SportsRuHelper.cs
@@ -1,36 +1,207 @@
 ﻿using System;
 using System.Linq;
+using System.Threading;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
 
 namespace TestSandbox1.Tests.Helpers
 {
     public static class SportsRuHelper
     {
+        public static string GetRandomEmail()
+        {
+            return Guid.NewGuid().ToString("d").Substring(0, 8).ToLower() + "@" +
+                Guid.NewGuid().ToString("d").Substring(0, 6).ToLower() + ".0" +
+                Guid.NewGuid().ToString("d").Substring(0, 2).ToLower();
+        }
+
         public static void RegSportsRu(this IWebDriver d, string sportsUrl, string email, string password, string username)
         {
             d.Navigate().GoToUrl(sportsUrl);
 
+            var wait = new WebDriverWait(d, TimeSpan.FromSeconds(30));
             wait.Until(ExpectedConditions.ElementIsVisible(By.PartialLinkText("Зарегистрироваться")));
             d.FindElement(By.PartialLinkText("Зарегистрироваться")).Click();
 
-            //wait.Until(ExpectedConditions.ElementIsVisible(By.Name("login")));
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("(.//input[contains(@placeholder,'Ваш e-mail')])[2]")));
+            d.FindElement(By.XPath("(.//input[contains(@placeholder,'Ваш e-mail')])[2]")).SendKeys(email);
+            d.FindElement(By.XPath("(.//input[contains(@placeholder,'Имя на сайте')])[1]")).SendKeys(username);
+            d.FindElement(By.XPath("(.//input[contains(@placeholder,'Пароль')])[1]")).SendKeys(password);
+            d.FindElement(By.XPath("(.//input[contains(@placeholder,'Пароль еще раз')])[1]")).SendKeys(password);
+            d.FindElement(By.XPath("(.//div/button[contains(@type, 'submit')])[2]")).Click();
+            
+            Thread.Sleep(2000);
 
-            Console.WriteLine("1 " + d.FindElements(By.ClassName("popup")).Count);
-            Console.WriteLine("2 " + d.FindElements(By.ClassName("popup")).First().FindElements(By.Name("login")).Count);
-            Console.WriteLine("3 " + d.FindElements(By.ClassName("popup")).Count(x => x.GetAttribute("value").Contains("Ваш e-mail")));
-            Console.WriteLine("4 " + d.FindElements(By.ClassName("popup")).Count(x => x.GetAttribute("placeholder").Contains("Ваш e-mail")));
+            Console.WriteLine("{0} {1}", email, password);
 
-            d.FindElement(By.Name("login")).Click();
-            d.FindElement(By.Name("login")).SendKeys(email);
-            d.FindElement(By.Name("nick")).Click();
-            d.FindElement(By.Name("nick")).SendKeys(username);
-            d.FindElement(By.Name("password")).Click();
-            d.FindElement(By.Name("rassword")).SendKeys(password);
-            d.FindElement(By.Name("repasswd")).Click();
-            d.FindElement(By.Name("repasswd")).SendKeys(password);
+        }
 
-            d.FindElements(By.TagName("button")).First(x => x.GetAttribute("value").Contains("Зарегистрироваться")).Click();
+        public static void LoginSportsRu(this IWebDriver d, string email, string password)
+        {
+            d.Navigate().GoToUrl("http://www.sports.ru/logon.html");
+
+            var wait = new WebDriverWait(d, TimeSpan.FromSeconds(45));
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath(".//div/input[contains(@id, 'login')]")));
+
+            Thread.Sleep(2000);
+            d.FindElement(By.XPath(".//div/input[contains(@id, 'login')]")).SendKeys(email);
+            Thread.Sleep(2000);
+            d.FindElement(By.XPath(".//div/input[contains(@id, 'password')]")).SendKeys(password);
+            Thread.Sleep(2000);
+            d.FindElement(By.XPath(".//div/input[contains(@id, 'login')]")).Click();
+            Thread.Sleep(3000);
+
+            Console.WriteLine("Entered: {0} {1}", email, password);
+
+            d.FindElement(By.XPath(".//div/input[contains(@name, 'submit')]")).Click();
+
+        }
+
+        public static void TryLoginSportsRu(this IWebDriver d, string email, string password)
+        {
+            var wait = new WebDriverWait(d, TimeSpan.FromSeconds(45));
+            try
+            {
+                d.Navigate().GoToUrl("http://www.sports.ru/logon.html");
+
+                wait.Until(ExpectedConditions.ElementIsVisible(By.XPath(".//div/input[contains(@id, 'login')]")));
+
+                Thread.Sleep(2000);
+                d.FindElement(By.XPath(".//div/input[contains(@id, 'login')]")).SendKeys(email);
+                Thread.Sleep(2000);
+                d.FindElement(By.XPath(".//div/input[contains(@id, 'password')]")).SendKeys(password);
+                Thread.Sleep(2000);
+                d.FindElement(By.XPath(".//div/input[contains(@id, 'login')]")).Click();
+                Thread.Sleep(3000);
+
+                Console.WriteLine("Entered: {0} {1}", email, password);
+
+                d.FindElement(By.XPath(".//div/input[contains(@name, 'submit')]")).Click();
+            }
+            catch (Exception)
+            {
+                var username = email.Split('@').First();
+                wait.Until(ExpectedConditions.ElementIsVisible(By.XPath(
+                    string.Format(".//a[text()='{0}']", username))));
+                
+            }
+            
+
+        }
+        
+        public static void LikeLastPosts(this IWebDriver d, string url)
+        {
+            d.Navigate().GoToUrl(url);
+
+            var wait = new WebDriverWait(d, TimeSpan.FromSeconds(30));
+            wait.Until(ExpectedConditions.ElementIsVisible(By
+                .XPath("(.//div[contains(@class, 'voting')]/i[contains(@class, 'plus')])[1]")));
+            d.FindElement(By
+                .XPath("(.//div[contains(@class, 'voting')]/i[contains(@class, 'plus')])[1]"))
+                .Click();
+        }
+
+        public static void LikeComments(this IWebDriver d, string pageUrl)
+        {
+            d.Navigate().GoToUrl(pageUrl);
+
+            var wait = new WebDriverWait(d, TimeSpan.FromSeconds(30));
+            var plusXpath = ".//li[contains(@class, 'comments-list__item')]//div/" +
+                            "a[contains(@title, 'Teddy Sheringham')]/../..//div[contains(@class, 'rating')]/" +
+                            "div/a[contains(@class, 'plus')]/span";
+            
+            var js = (IJavaScriptExecutor)d;
+            js.ExecuteScript("window.scrollTo(0, document.body.scrollHeight);");
+
+            var footer = d.FindElement(By.XPath(".//footer[@class = 'main-footer']"));
+            var actions = new Actions(d);
+
+            Thread.Sleep(2000);
+            actions.MoveToElement(footer);
+            Thread.Sleep(2000);
+
+            var pluses = d.FindElements(By.XPath(plusXpath));
+            Console.WriteLine(pluses.Count + " comments to like");
+            foreach (var plus in pluses)
+            {
+                try
+                {
+                    Console.Write("Clicking... ");
+                    actions.MoveToElement(plus).Build().Perform();
+                    Thread.Sleep(2000);
+                    actions.MoveToElement(plus).Build().Perform();
+                    Thread.Sleep(3000);
+                    js.ExecuteScript("window.scrollBy(0, -100)", "");
+                    Thread.Sleep(3000);
+                    actions.Click(plus).Perform();
+                    //actions.MoveToElement(plus).Click().Perform();
+                    //var point = plus.Location;
+                    //actions.MoveByOffset(point.X, point.Y).Click().Perform();
+                    //plus.Click();
+                    Console.WriteLine(" done.");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine(" NOT done.");
+                }
+            }
+        }
+
+        public static void LikeLastTenPosts(this IWebDriver d, string url)
+        {
+            //d.Navigate().GoToUrl(url);
+
+            //var wait = new WebDriverWait(d, TimeSpan.FromSeconds(30));
+            //wait.Until(ExpectedConditions
+            //    .ElementIsVisible(By
+            //    .XPath(".//div[contains(@class, 'voting')]/i[contains(@class, 'plus')]")));
+            
+            var pluses = d.FindElements(By.XPath(".//div[contains(@class, 'voting')]/i[contains(@class, 'plus')]"));
+            foreach (var plus in pluses)
+            {
+                try
+                {
+                    var js = (IJavaScriptExecutor)d;
+                    var actions = new Actions(d);
+                    Thread.Sleep(500);
+                    actions.MoveToElement(plus).Build().Perform();
+                    Thread.Sleep(500);
+                    js.ExecuteScript("window.scrollBy(0, -100)", "");
+                    Thread.Sleep(500);
+                    actions.Click(plus).Perform();
+                    //plus.Click();
+                    Thread.Sleep(200);
+                }
+                catch (Exception)
+                {
+                    Thread.Sleep(100);
+                }
+            }
+        }
+
+        public static void Subscribe(this IWebDriver d, string url)
+        {
+            d.Navigate().GoToUrl(url);
+            var actions = new Actions(d);
+            var wait = new WebDriverWait(d, TimeSpan.FromSeconds(30));
+            wait.Until(ExpectedConditions.ElementIsVisible(By
+                .XPath("(.//span[contains(@class, 'like')]/i[contains(text(), 'Подписаться на блог')])")));
+
+            actions.MoveToElement(d.FindElement(By
+                .XPath("(.//span[contains(@class, 'like')]/i[contains(text(), 'Подписаться на блог')])")))
+                .Build().Perform();
+            Thread.Sleep(1000);
+            var js = (IJavaScriptExecutor)d;
+            js.ExecuteScript("window.scrollBy(0, -100)", "");
+            Thread.Sleep(1000);
+            actions.Click(d.FindElement(By
+                .XPath("(.//span[contains(@class, 'like')]/i[contains(text(), 'Подписаться на блог')])")))
+                .Perform();
+
+            //d.FindElement(By
+            //    .XPath("(.//span[contains(@class, 'like')]/i[contains(text(), 'Подписаться на блог')])")).Click();
+            
         }
     }
 }
--- TestSandbox1/TestSandbox1/Tests/TestSite.cs
+++ TestSandbox1/TestSandbox1/Tests/TestSite.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 using TestSandbox1.Tests.Helpers;
 
@@ -12,77 +15,271 @@ namespace TestSandbox1.Tests
     [TestFixture]
     public class TestSite
     {
-        internal IWebDriver D;
+        public IWebDriver D { get; set; }
+        public Process TorProcess { get; set; }
+        public WebDriverWait Wait { get; set; }
 
-        [SetUp]
         public void SetupTest()
         {
-            D = new ChromeDriver();
+            const string torBinaryPath = @"C:\TorBrowser\Browser\firefox.exe";
+            TorProcess = new Process
+            {
+                StartInfo =
+                {
+                    FileName = torBinaryPath,
+                    Arguments = "-n",
+                    WindowStyle = ProcessWindowStyle.Maximized
+                }
+            };
+            TorProcess.Start();
+
+            var profile = new FirefoxProfile();
+            profile.SetPreference("network.proxy.type", 1);
+            profile.SetPreference("network.proxy.socks", "127.0.0.1");
+            profile.SetPreference("network.proxy.socks_port", 9150);
+            D = new FirefoxDriver(profile);
+            Wait = new WebDriverWait(D, TimeSpan.FromSeconds(60));
         }
 
-        [TearDown]
         public void TeardownTest()
         {
-            //D.Quit();
+            D.Quit();
+            TorProcess.Kill();
         }
 
         [Test]
-        public void TestSportsRu()
+        public void TryLoginSportsRu()
         {
-            Console.WriteLine("Test started!");
-            const string mailUrl = "https://10minutemail.net";
-            const string sportsUrl = "https://sports.ru/";
-            using (D)
+            var auths = AuthSportsRu.GetFullList();
+            foreach (var auth in auths)
             {
-                D.Navigate().GoToUrl(mailUrl);
+                try
+                {
+                    SetupTest();
+                    D.TryLoginSportsRu(auth.Email, auth.Password);
+                    TeardownTest();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine(auth.Email + " " + auth.Password + " not working");
+                }
+            }
+            
+        }
 
-                var wait = new WebDriverWait(D, TimeSpan.FromSeconds(30));
-                wait.Until(ExpectedConditions.ElementIsVisible(By.Id("fe_text")));
+        //[TestCase(1)]
+        //[TestCase(2)]
+        //[TestCase(3)]
+        public void Open_Tor_Browser(int s)
+        {
+            D.Navigate().GoToUrl(@"http://whatismyipaddress.com/");
+            var expression = By.XPath("//*[@id='section_left']/div[2]");
+            Wait.Until(x => x.FindElement(expression));
+            var element = D.FindElement(expression);
+            Console.WriteLine("84.40.65.000" + " " + element.Text);
+            Assert.AreNotEqual("84.40.65.000", element.Text);
+        }
 
-                D.FindElement(By.Id("fe_text")).Click();
-
-                var email = D.FindElement(By.Id("fe_text")).GetAttribute("value");
-                var username = email.Split('@').First();
-                var password = "www12342";
-
-                D.RegSportsRu(sportsUrl, email, password, username);
-                
-                D.Navigate().GoToUrl(mailUrl);
-
-                Thread.Sleep(5000);
+        //TODO: add random emails
+        //[TestCase(300)]
+        public void GetRegistrationsSportsRu(int count)
+        {
+            for (var i = 0; i < count; i++)
+            {
+                try
+                {
+                    const string sportsUrl = "https://sports.ru/";
+                    //using (D)
+                    //{
+                        var email = SportsRuHelper.GetRandomEmail();
+                        var username = email.Split('@').First();
+                        var password = Guid.NewGuid().ToString("d").Substring(0, 8).ToLower();
+                        D.RegSportsRu(sportsUrl, email, password, username);
+                    //}
+                }
+                catch (Exception)
+                {
+                    Thread.Sleep(500);
+                }
             }
         }
 
-        [Test]
-        public void Check()
+        [TestCase(500)]
+        public void GetRegistrationsForSportsRu(int count)
         {
-            Console.WriteLine("Test started!");
-            const string mailUrl = "https://10minutemail.net";
-            const string sportsUrl = "https://sports.ru/";
-            const string gitUrl = "https://github.com/join";
-            using (D)
+            for (var i = 0; i < count; i++)
             {
-                D.Navigate().GoToUrl(mailUrl);
+                SetupTest();
+                try
+                {
+                    const string mailUrl = "https://10minutemail.net";
+                    const string sportsUrl = "https://sports.ru/";
+                    using (D)
+                    {
+                        D.Navigate().GoToUrl(mailUrl);
+                        var wait = new WebDriverWait(D, TimeSpan.FromSeconds(30));
+                        wait.Until(ExpectedConditions.ElementIsVisible(By.Id("fe_text")));
+                        D.FindElement(By.Id("fe_text")).Click();
+                        var email = D.FindElement(By.Id("fe_text")).GetAttribute("value");
+                        var username = email.Split('@').First();
+                        var password = Guid.NewGuid().ToString("d").Substring(0, 8).ToLower();
+                        D.RegSportsRu(sportsUrl, email, password, username);
+                    }
+                }
+                catch (Exception)
+                {
+                    Thread.Sleep(500);
+                }
+                TeardownTest();
+            }
+        }
+        
+        [TestCase(10, "")]
+        [TestCase(10, " ")]
+        public void PutLikesToLastTenPost(int count, string url)
+        {
+            for (var i = 0; i < count; i++)
+            {
+                try
+                {
+                    //using (D)
+                    //{
+                        try
+                        {
+                            var a = AuthSportsRu.GetRandomItem();
+                            D.LoginSportsRu(a.Email, a.Password);
+                            Console.WriteLine("Log in done!");
+                            D.Subscribe(url);
+                            Console.WriteLine("Subscribed!");
+                        }
+                        catch
+                        {
+                            Thread.Sleep(500);
+                        }
+                        try
+                        {
+                            D.LikeLastTenPosts(url);
+                            Console.WriteLine("Post liked!");
+                        }
+                        catch
+                        {
+                            Thread.Sleep(500);
+                        }
+                    //}
+                }
+                catch (Exception)
+                {
+                    Thread.Sleep(500);
+                }
+            }
+        }
 
-                var wait = new WebDriverWait(D, TimeSpan.FromSeconds(30));
-                wait.Until(ExpectedConditions.ElementIsVisible(By.Id("fe_text")));
+        [TestCase(1)]
+        [TestCase(25)]
+        [TestCase(50)]
+        [TestCase(500)]
+        public void PutLikesToLastPost(int count)
+        {
+            var urls = new List<string>
+            {
+                //"http://www.sports.ru/tribuna/blogs/barcaonline/",
+                //"http://www.sports.ru/tribuna/blogs/smonline/",
+                //"http://www.sports.ru/tribuna/blogs/sportsweekend/",
+                "http://www.sports.ru/tribuna/blogs/funpics/"
+            };
 
-                D.FindElement(By.Id("fe_text")).Click();
+            for (var i = 0; i < count; i++)
+            {
+                try
+                {
+                    SetupTest();
+                    try
+                    {
+                        const string sportsUrl = "https://sports.ru/";
+                        var email = SportsRuHelper.GetRandomEmail();
+                        var username = email.Split('@').First();
+                        var password = Guid.NewGuid().ToString("d").Substring(0, 8).ToLower();
+                        D.RegSportsRu(sportsUrl, email, password, username);
+                        Console.WriteLine("Reg is done!");
+                        foreach (var url in urls)
+                        {
+                            try
+                            {
+                                D.Subscribe(url);
+                                Console.WriteLine("Subscribed!");
+                                D.LikeLastTenPosts(url);
+                                Console.WriteLine("Post liked!");
+                            }
+                            catch (Exception)
+                            {
+                                Thread.Sleep(500);
+                            }
+                        }
+                    }
+                    catch
+                    {
+                        Thread.Sleep(500);
+                    }
+                    TeardownTest();
+                }
+                catch (Exception)
+                {
+                    Thread.Sleep(500);
+                }
+            }
+        }
 
-                var email = D.FindElement(By.Id("fe_text")).GetAttribute("value");
-                var username = email.Split('@').First();
-                var password = "www12342";
-                Console.WriteLine("Email = '{0}'", email);
-                D.Navigate().GoToUrl(sportsUrl);
-                Thread.Sleep(10000);
-                D.Navigate().GoToUrl(gitUrl);
-                D.FindElement(By.Id("user_login")).SendKeys(username);
-                D.FindElement(By.Id("user_email")).SendKeys(email);
-                D.FindElement(By.Id("user_password")).SendKeys(password);
-                
-                D.Navigate().GoToUrl(mailUrl);
+        [TestCase(50, @"http://www.sports.ru/football/1037018186.html")]
+        [TestCase(50, @"http://www.sports.ru/football/1036882356.html")]
+        [TestCase(50, @"http://www.sports.ru/football/1037013785.html")]
+        [TestCase(50, @"http://www.sports.ru/football/1037023240.html")]
+        [TestCase(50, @"http://www.sports.ru/football/1037016222.html")]
+        [TestCase(50, @"http://www.sports.ru/football/1037020906.html")]
+        [TestCase(50, @"http://www.sports.ru/football/1037015816.html")]
+        [TestCase(50, @"http://www.sports.ru/football/146665972.html")]
+        public void PutLikesToComment(int count, string url)
+        {
+            for (var i = 0; i < count; i++)
+            {
+                //using (D)
+                //{
+                    try
+                    {
+                        var a = AuthSportsRu.GetRandomItem();
+                        D.LoginSportsRu(a.Email, a.Password);
+                        Console.WriteLine("Log in done!");
+                        D.LikeComments(url);
+                        Console.WriteLine("Comments liked!");
+                    }
+                    catch
+                    {
+                        Thread.Sleep(500);
+                    }
+                //}
+            }
+        }
 
-                Thread.Sleep(5000);
+        //[Test]
+        public void PutLikesToComment_Test()
+        {
+            for (var i = 0; i < 1; i++)
+            {
+                //using (D)
+                //{
+                    try
+                    {
+                        var a = AuthSportsRu.GetRandomItem();
+                        D.LoginSportsRu(a.Email, a.Password);
+                        Console.WriteLine("Log in done!");
+                        D.LikeComments("http://www.sports.ru/football/146665972.html");
+                    
+                        Console.WriteLine("Comments liked!");
+                    }
+                    catch
+                    {
+                        Thread.Sleep(500);
+                    }
+                //}
             }
         }
     }
